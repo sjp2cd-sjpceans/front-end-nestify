9860b2f2941bdb7dfea8e49545f25ab8187a6374
jayoadonis <jayo.adonisraphael@gmail.com>
2025-07-30 08:15:58 +0800

    [chore, change system promt]

--- src/services/api/ai/search.ts
+++ src/services/api/ai/search.ts
@@ -21,10 +21,53 @@ export async function aiSearch(query: string) {
     throw new Error('Failed to fetch data from backend that handled db');
   }
 
+  const formatData = JSON.stringify(`
+    {
+      "id": 1,
+      "name": "Stark Penthouse",
+      "description": "Lux penthouse in LA",
+      "size": "3500.00",
+      "price": "2500000.00",
+      "hadDiscount": 1,
+      "discountPrice": "2400000.00",
+      "propertyType": "Penthouse",
+      "status": "Available",
+      "address": {
+        "unitNumber": "Apt 101",
+        "street": "123 Elm St",
+        "city": "Los Angeles",
+        "zipCode": "90001",
+        "country": "United States",
+        "latitude": "9.99999999",
+        "longitude": "-9.99999999"
+      },
+      "agent": {
+        "id": 1,
+        "firstName": "Tony",
+        "lastName": "Stark",
+        "email": "tony.stark@starkindustries.com",
+        "phone": "555-IRON2"
+      },
+      "images": [
+        {
+          "id": 1,
+          "url": "/test/asset/img/property/pent_house.jpg",
+          "altText": "Stark Penthouse",
+          "isPrimary": 1,
+          "format": "jpg"
+        }
+      ]
+    }, 
+    {
+      and more ...
+    }
+  `);
+
+  // console.log("Data to be used for AI search:", JSON.stringify(data));
   //REM: Yikes...
   const body: IRequest = {
     type: 'search-engine',
-    system: { content: `Answering the "user" request should follow the constraint:\n* These are the datapoints:\n${JSON.stringify(data)}\nOnly answer based on it.\n\n* Directly return only a JSON array of correspoding listing.\n* No more extra chat or text, no formating, no \`\`\`json.\n* Make it short and to the point.`},
+    system: { content: `Answering the "user" request should follow the constraint:\n* These are the datapoints:\n\`\`\`json\n${JSON.stringify(data,null,2)}\n\`\`\`\nOnly answer based on it.\n\n* Directly return only a JSON array of correspoding listing.\n* No more extra chat or text, no formating, no \`\`\`json.\n* Make it short and to the point.` },
     user: { content: query }
   };
 
